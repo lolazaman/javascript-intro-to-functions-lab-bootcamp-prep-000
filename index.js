1685e6de2f97b41def6ff113a86065f80364f4d4
Latifa Bakkali <lolazaman@gmail.com>
2018-12-25 17:10:20 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(STRING) {
+  return STRING.toLowerCase()
+}
+function logshout(hello) {
+  console.log('string'.toUpperCase());
+}
